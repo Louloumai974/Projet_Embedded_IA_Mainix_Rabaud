cf1a0fc69c7bae55d4f43a94c21dd4074130b8b5
Louloumai974 <62339802+Louloumai974@users.noreply.github.com>
2022-10-17 18:12:28 +0200

    tests

--- python/ConvertNPY.py
+++ python/ConvertNPY.py
@@ -1,64 +1,17 @@
-from PIL import Image
 import os, sys
-import cv2
+from IPython.display import display
+from IPython.display import Image as _Imgdis
+from PIL import Image
 import numpy as np
+from time import time
+from time import sleep
+folder = "../processed_esca_dataset/train/esca"
 
+onlyfiles = [f for f in os.listdir(folder) if os.path.isfile(os.path.join(folder, f))]
 
-# Path to image directory
-path1 = "./processed_esca_dataset/test/esca"
-dirs = os.listdir( path1 )
-dirs.sort()
-x_train=[]
+print("Working with {0} images".format(len(onlyfiles)))
+print("Image examples: ")
 
-def load_dataset():
-    # Append images to a list
-    for item in dirs:
-        if os.path.isfile(path1+item):
-            im = Image.open(path1+item).convert("RGB")
-            im = np.array(im)
-            x_train.append(im)
-
-if __name__ == "__main__":
-    
-    load_dataset()
-    
-    # Convert and save the list of images in '.npy' format
-    imgset=np.array(x_train)
-    np.save("x_test_esca.npy",imgset)
-
-
-# Path to image directory
-path = "./processed_esca_dataset/test/healthy"
-dirs = os.listdir( path )
-dirs.sort()
-x_train=[]
-
-def load_dataset():
-    # Append images to a list
-    for item in dirs:
-        if os.path.isfile(path+item):
-            im = Image.open(path+item).convert("RGB")
-            im = np.array(im)
-            x_train.append(im)
-
-if __name__ == "__main__":
-    
-    load_dataset()
-    
-    # Convert and save the list of images in '.npy' format
-    imgset=np.array(x_train)
-    np.save("x_test_healthy.npy",imgset)
-
-
-esca = np.load('x_test_esca.npy')
-healthy = np.load('x_test_healthy.npy')
-x_test = []
-
-x_test = np.append(esca,healthy)
-
-np.save('x_test.npy',x_test)
-
-y_test = np.full(len(esca),'1')
-y_test = np.append(y_test,np.full(len(healthy),'2')) 
-np.save('y_test.npy',y_test) 
-
+for i in range(40, 42):
+    print(onlyfiles[i])
+    display(_Imgdis(filename=folder + "/" + onlyfiles[i], width=240, height=320))
---
+++ python/test.py
@@ -0,0 +1,7 @@
+from PIL import Image
+import os, sys
+import cv2
+import numpy as np
+
+file = np.load('y_test.npy')
+print(file)
